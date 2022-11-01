a0919f8a367d9626e3930eb8a351eb6cbb7f6553
timtakcs <timur_takhtarov@houston.nae.school>
2022-10-31 22:53:23 -0600

    retraining nn on depth 0 evals + more perf stuff

--- chess-ai/engine/main.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include "Engine.hpp"
-
-int main() {
-    Engine engine("rn1qkb1r/ppp2ppp/4b3/3n4/5P2/2N5/PPPPQ2P/R1B1KBNR w KQkq - 2 8");
-    engine.play();
-}
--- chess-ai/engine/test.cpp
+++
@@ -1,6 +0,0 @@
-#include "Board.hpp"
-#include <iostream>
-
-int main() {
-    
-}
